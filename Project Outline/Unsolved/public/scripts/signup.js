28d5a1bcc2da564ec43149e17c130d7d463bf9cf
TeresaG <tmg.coder@gmail.com>
2018-09-21 12:17:13 -0700

    added username and password to signup html and js

--- Project Outline/Unsolved/public/scripts/signup.js
+++ Project Outline/Unsolved/public/scripts/signup.js
@@ -5,36 +5,50 @@ $(document).ready(function() {
     var signUpForm = $("form.signup");
     var emailInput = $("input#email-input");
     var passwordInput = $("input#password-input");
+    var firstNameInput = $("input#firstName-input");
+    var lastNameInput = $("input#lastName-input");
+    var userNameInput = $("input#userName-input");
+
+
   
-    // When the signup button is clicked, we validate the email and password are not blank
+    // When the signup button is clicked, we validate the none of the input fields blank
     signUpForm.on("submit", function(event) {
       event.preventDefault();
       var userData = {
         email: emailInput.val().trim(),
-        password: passwordInput.val().trim()
+        password: passwordInput.val().trim(),
+        firstName: firstNameInput.val().trim(),
+        lastName: lastNameInput.val().trim(),
+        userName: userNameInput.val().trim()
       };
   
-      if (!userData.email || !userData.password) {
+      if (!userData.email || !userData.password ||!userData.firstName || !userData.lastName || !userData.userName) {
         return;
       }
-      // If we have an email and password, run the signUpUser function
-      signUpUser(userData.email, userData.password);
+      // If all of the input fields have data run the signUpUser function
+      signUpUser(userData.email, userData.password, userData.firstName, userData.lastName, userData.userName);
       emailInput.val("");
       passwordInput.val("");
+      firstNameInput.val("");
+      lastNameInput.val("");
+      userNameInput.val("");
     });
   
     // Does a post to the signup route. If successful, we are redirected to the members page
     // Otherwise we log any errors
-    function signUpUser(email, password) {
+    function signUpUser(email, password, firstName, lastName, userName) {
       $.post("/api/signup", {
         email: email,
-        password: password
+        password: password,
+        firstName: firstName,
+        lastName: lastName,
+        userName: userName
       }).then(function(data) {
         window.location.replace(data);
         // If there's an error, handle it by throwing up a bootstrap alert
       }).catch(handleLoginErr);
     }
-  
+    console.log(lastNameInput);
     function handleLoginErr(err) {
       $("#alert .msg").text(err.responseJSON);
       $("#alert").fadeIn(500);
