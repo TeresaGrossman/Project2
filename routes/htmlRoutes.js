2be0d2d844ee60c446cf7904c77217df3e98de1a
TeresaG <tmg.coder@gmail.com>
2018-09-22 12:31:23 -0700

    updated htmlRoutes to res.sendFile instead of render and added jawsDB

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -12,7 +12,7 @@ module.exports = function(app) {
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
+      res.sendFile("example", {
         example: dbExample
       });
     });
@@ -20,6 +20,6 @@ module.exports = function(app) {
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
-    res.render("404");
+    res.sendFile("404");
   });
 };
